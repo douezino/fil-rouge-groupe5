56a2cc2c856b0cd6c54c93899a62825ba54fb626
Diallo75012 <11600022@students.koi.edu.au>
2022-09-07 11:37:45 +0200

    changed gunicorn listening port to match openshift container native port

--- app.py
+++ app.py
@@ -5,5 +5,5 @@ import os
 # this will use the dtabase seetting for openshift posgresql database already deployed
 os.system("./manage.py migrate --database=openshiftpostgresql")
 
-# this will use gunicorn to run the server ans start the app that will serve on port 8000
-os.system("gunicorn --env DJANGO_SETTINGS_MODULE=webApp.settings webApp.wsgi")
+# this will use gunicorn to run the server ans start the app that will serve on port 8000 natively but changed to prot 8080 with option -b
+os.system("gunicorn --env DJANGO_SETTINGS_MODULE=webApp.settings webApp.wsgi -b :8080")
