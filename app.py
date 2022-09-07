1da23f1a634e7f66b1c5d8f97112b2b28c4999a0
Majid <majid.ababou@gmail.com>
2022-09-07 16:08:39 +0200

    Test Manage.py

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ import os
 
 
 # this will use the dtabase seetting for openshift posgresql database already deployed
-os.system("./manage.py --help")
-#os.system("./manage.py migrate --database=openshiftpostgresql")
+os.system("sudo ./manage.py migrate --database=openshiftpostgresql")
 
 # this will use gunicorn to run the server ans start the app that will serve on port 8000 natively but changed to prot 8080 with option -b
 os.system("gunicorn --env DJANGO_SETTINGS_MODULE=webApp.settings webApp.wsgi -b :8080")
