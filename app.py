e48381bf1e3eca422fc5c943ab91a1f12219ff48
Diallo75012 <11600022@students.koi.edu.au>
2022-09-09 01:40:15 +0200

    changed tests files and added gitignore

--- app.py
+++ app.py
@@ -11,5 +11,8 @@ superuser="from django.contrib.auth.models import User; User.objects.create_supe
 # create superuser django no input
 os.system("python3 manage.py shell -c "${superuser}"")
 
+# create the log file
+os.system("touch logs_webApp.log")
+
 # this will use gunicorn to run the server ans start the app that will serve on port 8000 natively but changed to prot 8080 with option -b
 os.system("gunicorn --env DJANGO_SETTINGS_MODULE=webApp.settings webApp.wsgi -b :8080")
