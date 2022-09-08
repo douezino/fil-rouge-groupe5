19d2706620982085ee88b86c80b32305c9be15a5
Diallo75012 <11600022@students.koi.edu.au>
2022-09-09 00:58:00 +0200

    have set a special settings to run tests locally and set django github action test. Have also set sonarqube test for github actions. The goal is to test before deployment to openshift. Have also set creation of suer user that need to change password when admin connects via ssh and before creation route in openshift.

--- app.py
+++ app.py
@@ -5,5 +5,11 @@ import os
 # this will use the dtabase seetting for openshift posgresql database already deployed
 os.system("python3 ./manage.py migrate")
 
+# connect to pod to change password before setting route for pod
+superuser="from django.contrib.auth.models import User; User.objects.create_superuser('djangoadmin', 'djangoadmin@example.com', 'm2iformation')"
+
+# create superuser django no input
+os.system("python3 manage.py shell -c "${superuser}"")
+
 # this will use gunicorn to run the server ans start the app that will serve on port 8000 natively but changed to prot 8080 with option -b
 os.system("gunicorn --env DJANGO_SETTINGS_MODULE=webApp.settings webApp.wsgi -b :8080")
