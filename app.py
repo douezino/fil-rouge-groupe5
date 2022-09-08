55ddc652aac196a21486376c4862ad4bcf74f543
Majid <majid.ababou@gmail.com>
2022-09-08 09:32:55 +0200

    TEST

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import os
 
 
-# this will use the dtabase seetting for openshift posgresql database already deployed
+# this will use the dtabase seetting for openshift posgresql database already deployedd
 os.system("python3 ./manage.py migrate --database=openshiftpostgresql")
 
 # this will use gunicorn to run the server ans start the app that will serve on port 8000 natively but changed to prot 8080 with option -b
